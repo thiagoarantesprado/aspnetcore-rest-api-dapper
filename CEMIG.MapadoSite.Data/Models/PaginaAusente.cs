6082087cd6621ea214ac57751fc8d12d324e71f9
Thiago Prado <thiago.prado@bhs.com.br>
2019-10-07 16:45:22 -0300

    Finalizando validações de formulários

--- CEMIG.MapadoSite.Data/Models/PaginaAusente.cs
+++ CEMIG.MapadoSite.Data/Models/PaginaAusente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CEMIG.MapadoSite.Data.Models
@@ -7,7 +8,10 @@ namespace CEMIG.MapadoSite.Data.Models
     public class PaginaAusente
     {
             public int Id { get; set; }
+            [Required]
             public string Link { get; set; }
+            [Display(Name = "Observação", Description = "Observação sobre o site ausente.")]
+            [Required(ErrorMessage = "A observação é obrigatória.")]
             public string Observacao { get; set; }
             public string Usuario { get; set; }
             public DateTime DataCriacao { get; set; }
--- CEMIG.MapadoSite/Controllers/HomeController.cs
+++ CEMIG.MapadoSite/Controllers/HomeController.cs
@@ -158,11 +158,10 @@ namespace CEMIG.MapadoSite.Controllers
             if (ModelState.IsValid)
             {
                 _menuBusiness.AddPaginaAusente(paginaAusente);
-
                 return RedirectToAction("PaginasAusente");
             }
             else
-                return View();
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
